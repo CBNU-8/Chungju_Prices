36d707dec50e5ecea39a87ea5822d9fb518d76c4
jeongmin99 <80687137+jeongmin99@users.noreply.github.com>
2021-06-05 18:25:43 +0900

    Add files via upload

--- asd.py
+++ asd.py
@@ -1,20 +1,23 @@
-import sys
+import sys#시스템 모듈
+import os#OS 모듈
+from PyQt5 import uic#pyqt를 위한 모듈
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
-import pymysql #따로 설치해주어야한다.
-import matplotlib.pyplot as plt
+import pymysql #mysql 쿼리문을 python에서 사용하게하는 라이브러리
+import matplotlib.pyplot as plt#matplotlib import
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import json
+
 
 connect = pymysql.connect(host='localhost', user='hi', password='asd123', db='new_schema',charset='utf8mb4')
 cur = connect.cursor()
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("untitled.ui")[0]
+form_class = uic.loadUiType("0603UI.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
-class WindowClass(QMainWindow, form_class) :
+class WindowClass(QMainWindow, form_class) :    
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
@@ -23,8 +26,14 @@ class WindowClass(QMainWindow, form_class) :
         self.place.cellClicked.connect(self.placePricediff)
         self.pummok.cellClicked.connect(self.pummokPricediff)
         self.setFixedSize(1280, 720)
-        self.setWindowTitle("Pricer")
-        
+        self.setWindowTitle("청주 물가 바로보기")
+        self.setupMap()
+    
+    
+    def setupMap(self):
+        os.remove("jijum.json")
+
+       
     def setuptableUI(self):
         cur.execute("SELECT COUNT(DISTINCT 상품명) FROM new_schema.asd")
         result=cur.fetchone()
@@ -58,21 +67,19 @@ class WindowClass(QMainWindow, form_class) :
         self.place.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
         
     def setpummokTableWidgetData(self):
-       i = 0
-       
-       column_headers = ['상품명', '제조사']
-       self.pummok.setHorizontalHeaderLabels(column_headers)
+        i = 0
         
-       query="SELECT DISTINCT 상품명, 제조사 FROM new_schema.asd;"
-       cur.execute(query)
-       connect.commit()
+        column_headers = ['상품명', '제조사']
+        self.pummok.setHorizontalHeaderLabels(column_headers)
         
-       datas = cur.fetchall()
-       for data in datas:
+        query="SELECT DISTINCT 상품명 FROM new_schema.asd;"
+        cur.execute(query)
+        connect.commit()
+        
+        datas = cur.fetchall()
+        for data in datas:
             pummokname=data[0] 
-            companyname=data[1]
             self.pummok.setItem(i , 0, QTableWidgetItem(pummokname))
-            self.pummok.setItem(i , 1, QTableWidgetItem(companyname))
             i += 1
  
     def setcompanyTableWidgetData(self):
@@ -81,7 +88,7 @@ class WindowClass(QMainWindow, form_class) :
         column_headers = ['제조사']
         self.company.setHorizontalHeaderLabels(column_headers)
         
-        query="SELECT DISTINCT 제조사 FROM new_schema.asd;" 
+        query="SELECT DISTINCT 제조사 FROM new_schema.asd;"
         cur.execute(query)
         connect.commit()
         
@@ -100,7 +107,7 @@ class WindowClass(QMainWindow, form_class) :
         query="SELECT DISTINCT 판매업소 FROM new_schema.asd;"
         cur.execute(query)
         connect.commit()
-                
+        
         datas = cur.fetchall()
         for data in datas:
             placename=data[0] 
@@ -124,7 +131,7 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
@@ -140,13 +147,14 @@ class WindowClass(QMainWindow, form_class) :
             self.companydifflabel.setText(str(round((nowyear-pastyear)/pastyear*100,2))+'%')
         else: 
           self.companydifflabel.clear()
-          self.companydifflabel.setText("작년/올해 데이터가 없습니다.") 
+          self.companydifflabel.setText("작년/올해 데이터가 없습니다.")  
         
         self.showcompanygraph()            
             
     def pummokPricediff(self):
         pastyear=0
         nowyear=0
+        avgprice=0
        
         for i in range(2020,2022):
             count=0
@@ -161,7 +169,7 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
@@ -171,23 +179,24 @@ class WindowClass(QMainWindow, form_class) :
                 pastyear=avg
             else:
                 nowyear=avg
-                
-        if nowyear!=None:
-            self.pricelabel.clear()
-            self.pricelabel.setText(str(round(nowyear))+'원') 
-        else:
-            self.pricelabel.clear()
-            self.pricelabel.setText("올해 데이터가 없습니다.")
-
+                if nowyear==None:
+                    self.pricelabel.clear()
+                    self.pricelabel.setText("올해 데이터가 없습니다.")
+                else:
+                    self.pricelabel.clear()
+                    self.pricelabel.setText(str(round(nowyear))+'원')
+                    
+    
+         
         if pastyear!=None and nowyear!=None:
             self.pummokdifflabel.clear()
             self.pummokdifflabel.setText(str(round((nowyear-pastyear)/pastyear*100,2))+'%')
         else: 
-            self.pummokdifflabel.clear()
-            self.pummokdifflabel.setText("작년/올해 데이터가 없습니다.") 
+          self.pummokdifflabel.clear()
+          self.pummokdifflabel.setText("작년/올해 데이터가 없습니다.") 
           
-        self.showpummokGraph()  
-                         
+        self.showpummokGraph()                           
+
     def showcompanygraph(self):
         distance=[]
         pastyear=0
@@ -211,7 +220,7 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
@@ -234,7 +243,7 @@ class WindowClass(QMainWindow, form_class) :
                     distance.append((nowyear-pastyear)/pastyear*100)
             
         print(distance)
-        
+
         ax = self.fig.add_subplot(111)
         ax.plot(['2014','2015','2016','2017','2018','2019','2020','2021'],distance,marker='o')
         ax.set_xlabel("year")
@@ -247,9 +256,9 @@ class WindowClass(QMainWindow, form_class) :
         yearprice=[]
         
         self.fig = plt.Figure()
-        self.canvas = FigureCanvas(self.fig)
+        self.canvas = FigureCanvas(self.fig) 
         self.pummokgraph.addWidget(self.canvas)
-        
+
         for i in range(2014,2022):
             count=0
             hap=0
@@ -263,7 +272,7 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
@@ -272,7 +281,7 @@ class WindowClass(QMainWindow, form_class) :
             
             print(avg)
             yearprice.append(avg)
-            
+
         ax = self.fig.add_subplot(111)
         ax.plot(['2014','2015','2016','2017','2018','2019','2020','2021'],yearprice,marker='o')
         ax.set_xlabel("year")
@@ -282,9 +291,25 @@ class WindowClass(QMainWindow, form_class) :
         self.pummokgraph.removeWidget(self.canvas)
    
     def placePricediff(self):
+        
+        query="select distinct 판매업소 from new_schema.asd where 판매업소 like '%s';"%self.place.item(self.place.currentRow(),0).text()
+        cur.execute(query)
+        connect.commit()
+
+        datas = cur.fetchall()
+
+       
+        info=[]
+        for data in datas:
+            with open("jijum.json", "w", encoding='utf-8') as json_file:
+                info.append({"판매업소":data[0]})
+                json.dump(info, json_file,ensure_ascii=False)
+
+        self.webEngineView.reload()
+        
         pastyear=0
         nowyear=0
-        
+
         for i in range(2020,2022):
             count=0
             hap=0
@@ -298,7 +323,7 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
@@ -325,7 +350,7 @@ class WindowClass(QMainWindow, form_class) :
         
         self.fig = plt.Figure()
         self.canvas = FigureCanvas(self.fig)
-        self.placegraph.addWidget(self.canvas)
+        self.placegraph.addWidget(self.canvas) 
         
         for i in range(2014,2022):
             count=0
@@ -340,15 +365,17 @@ class WindowClass(QMainWindow, form_class) :
             for data in datas:
                 hap+=data[0]
                 count+=1
-                
+
             if count==0:
                 avg=None
             else:
-                avg=hap/count   
+                avg=hap/count
+               
             
             if i==2014:
                 nowyear=avg
                 distance.append(0)
+            
             else:
                 pastyear=nowyear
                 nowyear=avg
@@ -362,21 +389,24 @@ class WindowClass(QMainWindow, form_class) :
                     distance.append((nowyear-pastyear)/pastyear*100)
             
         print(distance)
-        
+
         ax = self.fig.add_subplot(111)
         ax.plot(['2014','2015','2016','2017','2018','2019','2020','2021'],distance,marker='o')
         ax.set_xlabel("year")
         ax.set_ylabel("Growth rate compared to last year")
         ax.legend()
         self.canvas.draw()
-        self.placegraph.removeWidget(self.canvas)     
+        self.placegraph.removeWidget(self.canvas)   
        
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv) 
+
     #WindowClass의 인스턴스 생성
     myWindow = WindowClass() 
+
     #프로그램 화면을 보여주는 코드
     myWindow.show()
+
     #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
     app.exec_()
